fa815d8807965e0fd1490f60dcd5d334463a018d
JiaYee <lowjiayee123@gmail.com>
2020-01-18 22:05:30 +0800

    Add resume.html

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route('/resume.html')
+def resume():
+	return render_template("resume.html")
+
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
